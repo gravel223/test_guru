1916f9893c2f43f37975815331c9389553f7cb4b
mazdai <mazda4697@gmail.com>
2021-08-10 18:22:03 +0400

    Добавил данные для модели tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.create([{ title: :frotend }, { title: :backend }, { title: :Database }])
+
+ tests = Test.create(
+  [{ title: 'Ruby', level: 1, category_id: categories[1].id },
+   { title: 'Ruby on rails', level: 2, category_id: categories[1].id },
+   { title: 'SQl', level: 2, category_id: categories[2].id },
+   { title: 'php', level: 1, category_id: categories[1].id},
+   { title: 'laravel', level: 2, category_id: categories[1].id }]
+   { title: 'javaScript', level: 1, category_id: categories[1].id }]
