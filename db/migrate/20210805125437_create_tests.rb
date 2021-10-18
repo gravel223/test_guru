8fad7033997919469bbfa3621e1aad4a53babb5a
mazdai <mazda4697@gmail.com>
2021-10-18 14:51:31 +0400

    миграция

--- db/migrate/20210805125437_create_tests.rb
+++ db/migrate/20210805125437_create_tests.rb
@@ -3,7 +3,6 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title
       t.integer :level
-      t.integer :author_id
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
