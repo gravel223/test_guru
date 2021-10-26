bb9594aaf1bbdc71ec81dde54a8f570abfc4b218
mazdai <mazda4697@gmail.com>
2021-10-26 15:49:03 +0400

    колличество ответов

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,16 @@
 class Question < ApplicationRecord
+  MAX_ANSWERS = 4
+
   belongs_to :test
   has_many :answers
 
   validates :body, presence: true
+  validate :answer_count, on: :create
+  private
+
+  def answer_count
+    message = 'Превышенно колличество ответов!'
+    errors.add(:question, message) if question.answers.count >= MAX_ANSWERS
+  end
 
 end
