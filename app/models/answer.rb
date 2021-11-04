44166a3ea5cf635ecb792f24e558aa2e84b6825a
mazdai <mazda4697@gmail.com>
2021-11-04 17:16:34 +0400

    answers_count в Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,16 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+  validate :answers_count, on: :create
 
-  scope :true_answers -> { where(correct: true) }
+  scope :true_answers, -> { where(correct: true) }
+
+
+  private
+
+  def answers_count
+    message = 'превышено количество ответов!!'
+    errors.add(:question, message) if question.answers.count >= 4
+  end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,7 @@
 class Question < ApplicationRecord
-  MAX_ANSWERS = 4
-
   belongs_to :test
   has_many :answers
 
   validates :body, presence: true
-  validate :answer_count, on: :create
-  private
-
-  def answer_count
-    message = 'Превышенно колличество ответов!'
-    errors.add(:question, message) if question.answers.count >= MAX_ANSWERS
-  end
 
 end
