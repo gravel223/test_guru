99a8ed75e1640d2f460eec80ccf3fd83e55f0be4
mazdai <mazda4697@gmail.com>
2021-11-30 16:54:11 +0400

    add

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,16 +2,24 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @questions = @test.questions
   end
 
   def create
-    render plain: @question.body
+    question = @test.questions.new(question_params)
+
+    if question.save
+      render plain: "Question '#{question_params[:body]}' created"
+    else
+      render plain: 'Creating error'
+    end
   end
 
   def show
-
+    ender plain: @question.body
   end
 
   def destroy
@@ -29,6 +37,14 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 
 end
