eb29493fe4523616d58753c9dfb5286e0ab4a55e
mazdai <mazda4697@gmail.com>
2021-12-08 16:29:24 +0400

    test index

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,23 @@
 class TestsController < ApplicationController
   def index
-     render plain: Test.all.inspect
+    @tests = Test.all
   end
+
+  def destroy
+    @tests.destroy
+
+    redirect_to test_path
+  end
+
+  private
+
+  def test_find
+    @tests = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,7 @@
 module TestsHelper
+  TEST_LEVELS = { 0 => :easy, 1 => :elemetary, 2 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
 end
