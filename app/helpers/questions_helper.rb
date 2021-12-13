6202578a32b03ad421017624858b277eaf3bf3e6
mazdai <mazda4697@gmail.com>
2021-12-13 15:59:18 +0400

    QuestionsHelper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,8 @@
 module QuestionsHelper
+  module QuestionsHelper
+  def question_header(question)
+    header = question.new_record? ? 'Create New' : 'Edit'
+    header << " #{question.test.title} Question"
+  end
+end
 end
